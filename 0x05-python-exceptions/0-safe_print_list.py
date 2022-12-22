75bcc721af510eced9d68621dde17aca92294a99
Oluwatimileyin Aseperi <timmieash@gmail.com>
2022-12-21 18:38:58 -0800

    python exceptions corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
             num += 1
             for x in range(num):
                 print("", end="")
-            print()
-            return num
-        except IndexError:
-            print()
-            return num
+        print()
+        return num
+    except IndexError:
+        print()
+        return num
